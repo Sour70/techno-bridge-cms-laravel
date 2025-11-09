b106c9cdcdd0131f1cc82a56d29ed160006d9c4e
Sourabh Rahangdale <rahangdales13@gmail.com>
2025-11-09 21:59:29 +0530

    chnages

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
